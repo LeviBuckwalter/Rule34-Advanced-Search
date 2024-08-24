7bee4bbbc3fcb48c9091e89a58864d7ffce2242e
Levi <levibuck4@gmail.com>
2024-08-23 22:16:26 -0400

    improve mediumImage scaling

--- src/voteSearch.js
+++ src/voteSearch.js
@@ -380,6 +380,10 @@ function resetDisplay() {
         anchorEle.target = "_blank";
         const imageEle = document.createElement("img");
         imageEle.src = selectedPost.mediumImageUrl;
+        imageEle.style.maxWidth = "100vw";
+        imageEle.style.maxHeight = "80vh";
+        imageEle.style.width = "auto";
+        imageEle.style.height = "auto";
         anchorEle.appendChild(imageEle);
         imageDiv.appendChild(anchorEle);
     }
--- src/voteSearch.ts
+++ src/voteSearch.ts
@@ -455,6 +455,10 @@ function resetDisplay(): void {
         anchorEle.target = "_blank"
         const imageEle = document.createElement("img")
         imageEle.src = selectedPost.mediumImageUrl
+        imageEle.style.maxWidth = "100vw"
+        imageEle.style.maxHeight = "80vh"
+        imageEle.style.width = "auto"
+        imageEle.style.height = "auto"
 
         anchorEle.appendChild(imageEle)
         imageDiv.appendChild(anchorEle)
