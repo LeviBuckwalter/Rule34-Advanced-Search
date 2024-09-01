7e8dd6a0c397dc11fc56aa83c33a3302425bd485
Levi <levibuck4@gmail.com>
2024-09-01 18:38:38 -0400

    add PostDisplay and PostDisplayArray classes

---
+++ src/functions/PostDisplay.ts
@@ -0,0 +1,59 @@
+import { Post } from "../../R34-Tools/src/classes/Post";
+
+export class PostDisplay {
+    private post: Post
+    private spanEle: HTMLSpanElement
+    private imgEle: HTMLImageElement
+
+    constructor(post: Post, htmlParent: HTMLElement) {
+        this.post = post
+        this.spanEle = document.createElement("span")
+        htmlParent.appendChild(this.spanEle)
+        this.imgEle = document.createElement("img")
+        this.spanEle.appendChild(this.imgEle)
+
+        this.displaySmall()
+    }
+
+    displayBig() {
+        this.imgEle.src = this.post.mediumImageUrl
+
+        const passableThis = this
+        this.imgEle.addEventListener("click", function () {
+            passableThis.displaySmall()
+        })
+
+
+        //button that takes you to r34 website
+        const r34AnchorEle = document.createElement("a")
+        r34AnchorEle.href = this.post.siteUrl
+        r34AnchorEle.target = "_blank"
+        this.spanEle.appendChild(r34AnchorEle)
+        const r34ButtonEle = document.createElement("button")
+        r34ButtonEle.innerText = "Rule34.xxx"
+        r34AnchorEle.appendChild(r34ButtonEle)
+
+        //button for full image
+        const fullImageAnchorEle = document.createElement("a")
+        fullImageAnchorEle.href = this.post.fullImageUrl
+        fullImageAnchorEle.target = "_blank"
+        this.spanEle.appendChild(fullImageAnchorEle)
+        const fullImageButtonEle = document.createElement("button")
+        fullImageButtonEle.innerText = "Full Image"
+        fullImageAnchorEle.appendChild(fullImageButtonEle)
+
+    }
+
+    displaySmall() {
+        this.spanEle.replaceChildren()//remove all children
+        this.spanEle.appendChild(this.imgEle)
+
+        this.imgEle.src = this.post.thumbnailUrl
+        const passableThis = this
+        this.imgEle.addEventListener("click", function () {
+            passableThis.displayBig()
+        })
+    }
+
+
+}
---
+++ src/functions/PostDisplayArray.ts
@@ -0,0 +1,86 @@
+import { Post } from "../../R34-Tools/src/classes/Post.js";
+import { PostDisplay } from "./PostDisplay.js";
+
+type postDisplayOptions = {
+    postsPerPage?: number
+}
+
+export class PostDisplayArray {
+    public posts: Post[]
+    private divEle: HTMLDivElement
+    private postsPerPage: number
+    public currentPage: number
+
+    constructor(posts: Post[], htmlParent: HTMLElement, options: postDisplayOptions) {
+        this.posts = posts
+        this.divEle = document.createElement("div")
+        this.postsPerPage = (options.postsPerPage) ? options.postsPerPage! : 50
+        this.currentPage = 1
+
+        htmlParent.appendChild(this.divEle)
+    }
+
+    private get maxPages() {
+        return Math.ceil(this.posts.length / this.postsPerPage)
+    }
+
+    public display() {
+        this.divEle.replaceChildren()//clears children
+
+        console.log(this.maxPages, this.currentPage)
+
+        //next and previous page buttons: top
+        const passableThis = this
+        const buttonsDivTop = document.createElement("div")
+        this.divEle.appendChild(buttonsDivTop)
+        if (this.currentPage > 1) {
+            const pageBackward = document.createElement("button")
+            pageBackward.innerText = "Previous Page"
+            pageBackward.addEventListener("click", function () {
+                passableThis.currentPage--
+                passableThis.display()
+            })
+            buttonsDivTop.appendChild(pageBackward)
+        }
+        if (this.currentPage < this.maxPages) {
+            const pageForwardButton = document.createElement("button")
+            pageForwardButton.innerText = "Next Page"
+            pageForwardButton.addEventListener("click", function () {
+                passableThis.currentPage++
+                passableThis.display()
+            })
+            buttonsDivTop.appendChild(pageForwardButton)
+        }
+
+        //post displays
+        const startIndex = (this.currentPage - 1) * this.postsPerPage//inclusive
+        const endIndex = this.currentPage * this.postsPerPage//exclusive
+        const postsToDisplay = this.posts.slice(startIndex, endIndex)
+        for (const post of postsToDisplay) {
+            const pd = new PostDisplay(post, this.divEle)
+        }
+
+
+        //next and previous page buttons: bottom
+        const buttonsDivBottom = document.createElement("div")
+        this.divEle.appendChild(buttonsDivBottom)
+        if (this.currentPage > 1) {
+            const pageBackward = document.createElement("button")
+            pageBackward.innerText = "Previous Page"
+            pageBackward.addEventListener("click", function () {
+                passableThis.currentPage--
+                passableThis.display()
+            })
+            buttonsDivBottom.appendChild(pageBackward)
+        }
+        if (this.currentPage < this.maxPages) {
+            const pageForwardButton = document.createElement("button")
+            pageForwardButton.innerText = "Next Page"
+            pageForwardButton.addEventListener("click", function () {
+                passableThis.currentPage++
+                passableThis.display()
+            })
+            buttonsDivBottom.appendChild(pageForwardButton)
+        }
+    }
+}
