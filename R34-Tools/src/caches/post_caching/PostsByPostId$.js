81a5385cbf19de5621f2aefb3b6890f34f49d754
Levi <levibuck4@gmail.com>
2024-09-01 18:35:15 -0400

    reduce amount of posts held in cache

--- R34-Tools/src/caches/post_caching/PostsByPostId$.js
+++ R34-Tools/src/caches/post_caching/PostsByPostId$.js
@@ -2,7 +2,7 @@ import { Cache } from "../../../Cache/src/classes/Cache.js";
 /*
 stores posts, indexed by postIds.
 */
-export const PostsByPostId$ = new Cache("PostsByPostId$", 50000);
+export const PostsByPostId$ = new Cache("PostsByPostId$", 10000);
 PostsByPostId$.makeKey = function (postId) {
     return `${postId}`;
 };
